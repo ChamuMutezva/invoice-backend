8e08a652fe80e24bc36628c1fd890f56ecc1dce9
Chamunorwa Mutezva <ckmutezva@gmail.com>
2023-03-10 05:35:27 +0200

    routes initial setup

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
+require('dotenv').config();
 const express = require('express');
+const PORT = process.env.PORT;
+const invoicesRoutes = require('./routes/invoices')
 
+// app express
 const app = express()
 
+// middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+//routes
+app.use(('/api/invoices'),invoicesRoutes)
+
 // listen for requests
-app.listen(4000, () => {
-    console.log('listening to port 4000!!')
+app.listen(PORT, () => {
+    console.log(`listening to port ${PORT}`)
 })
