2be05563bde3fd6570e798ad6d29515a70d76fed
Chamunorwa Mutezva <ckmutezva@gmail.com>
2023-03-11 09:05:29 +0200

    data schema completed

--- models/invoices.js
+++ models/invoices.js
@@ -2,28 +2,54 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const invoicesSchema = new Schema(
+const invoiceSchema = new Schema(
   {
     id: {
       type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      required: true,
+    },
+    paymentDue: {
+      type: Date,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    paymentTerms: {
+      type: Number,
       required: true,
     },
     clientName: {
       type: String,
       required: true,
     },
-    dueDate: {
-      type: Date,
+    clientEmail: {
+      type: String,
       required: true,
     },
     status: {
       type: String,
       required: true,
     },
-    amount: {
+    senderAddress: {
+      type: Object,
+      required: true,
+    },
+    clientAddress: {
+      type: Object,
+      required: true,
+    },
+    total: {
       type: Number,
       required: true,
     },
   },
   { timestamps: true }
 );
+
+module.exports = mongoose.model("Invoice", invoiceSchema);
